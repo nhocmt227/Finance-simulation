1f0ad2b7602778055ac42c2acda78bfe19914b38
nhocmt227 <phong1572005@gmail.com>
2025-02-22 15:06:15 +0800

    Update requirement.txt

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,9 +1,4 @@
-import csv
 import datetime
-import pytz
-import requests
-import urllib
-import uuid
 
 from flask import redirect, render_template, session
 from functools import wraps
@@ -51,37 +46,37 @@ def login_required(f):
     return decorated_function
 
 
-def lookup(symbol):
-    """Look up quote for symbol."""
+# def lookup(symbol):
+#     """Look up quote for symbol."""
 
-    # Prepare API request
-    symbol = symbol.upper()
-    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
-    start = end - datetime.timedelta(days=7)
+#     # Prepare API request
+#     symbol = symbol.upper()
+#     end = datetime.datetime.now(pytz.timezone("US/Eastern"))
+#     start = end - datetime.timedelta(days=7)
 
-    # Yahoo Finance API
-    url = (
-        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
-        f"?period1={int(start.timestamp())}"
-        f"&period2={int(end.timestamp())}"
-        f"&interval=1d&events=history&includeAdjustedClose=true"
-    )
+#     # Yahoo Finance API
+#     url = (
+#         f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
+#         f"?period1={int(start.timestamp())}"
+#         f"&period2={int(end.timestamp())}"
+#         f"&interval=1d&events=history&includeAdjustedClose=true"
+#     )
 
-    # Query API
-    try:
-        response = requests.get(
-            url,
-            cookies={"session": str(uuid.uuid4())},
-            headers={"Accept": "*/*", "User-Agent": "python-requests"},
-        )
-        response.raise_for_status()
+#     # Query API
+#     try:
+#         response = requests.get(
+#             url,
+#             cookies={"session": str(uuid.uuid4())},
+#             headers={"Accept": "*/*", "User-Agent": "python-requests"},
+#         )
+#         response.raise_for_status()
 
-        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
-        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
-        price = round(float(quotes[-1]["Adj Close"]), 2)
-        return {"price": price, "symbol": symbol}
-    except (KeyError, IndexError, requests.RequestException, ValueError):
-        return None
+#         # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
+#         quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
+#         price = round(float(quotes[-1]["Adj Close"]), 2)
+#         return {"price": price, "symbol": symbol}
+#     except (KeyError, IndexError, requests.RequestException, ValueError):
+#         return None
 
 
 def usd(value):
