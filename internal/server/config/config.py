a39fafca84abc41b37f2595e78f8254b76c78b23
nhocmt227 <phong1572005@gmail.com>
2025-07-23 22:54:19 +0800

    Split config scope

--- internal/server/config/config.py
+++ internal/server/config/config.py
@@ -1,47 +1,7 @@
 import os
 import yaml
 import re
-
-class Config:
-    def __init__(self, app, database, core, api, test):
-        self.app = app
-        self.database = database
-        self.core = core
-        self.api = api
-        self.test = test
-
-class App:
-    def __init__(self, host, port, url, debug):
-        self.host = host
-        self.port = port
-        self.url = url
-        self.debug = debug
-
-class Database:
-    def __init__(self, db_name, db_type, timeout_second):
-        self.db_name = db_name
-        self.db_type = db_type
-        self.timeout_second = timeout_second
-
-class Core:
-    def __init__(self, logger):
-        self.logger = logger
-
-class Api:
-    def __init__(self, time_to_update_second):
-        self.time_to_update_second = time_to_update_second
-
-class Test:
-    def __init__(self, mock_boolean, mock_string, mock_integer, mock_float):
-        self.mock_boolean = mock_boolean
-        self.mock_string = mock_string
-        self.mock_integer = mock_integer
-        self.mock_float = mock_float
-
-class Logger:
-    def __init__(self, level, filename):
-        self.level = level
-        self.filename = filename
+from internal.server.config.config_object import Config, App, Database, Core, Api, Test, Logger
 
 CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../config/config.yaml'))
 ENV_VAR_PATTERN = re.compile(r"\$\{([^}]+)\}")
